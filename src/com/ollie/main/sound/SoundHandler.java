616dde165e808b9e9a4d105e978216f5a170edd4
weedle565 <oliverzross@hotmail.com>
2022-05-30 15:53:30 +1000

    Working animation, movement and game logic, work beginning on audio

---
+++ src/com/ollie/main/sound/SoundHandler.java
@@ -0,0 +1,14 @@
+package com.ollie.main.sound;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+
+public class SoundHandler {
+
+    private Clip clip;
+
+    private AudioInputStream inputStream;
+
+    private String path;
+
+}
